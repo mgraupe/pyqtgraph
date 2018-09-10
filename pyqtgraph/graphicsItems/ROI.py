fixed averaging for non-rectangular shaped roi regions.

pyqtgraph/graphicsItems/ROI.py
@@ -2050,11 +2050,12 @@ class PolyLineROI(ROI):
         Return the result of ROI.getArrayRegion(), masked by the shape of the 
         ROI. Values outside the ROI shape are set to 0.
         """
+        #print 'polyLine'
         br = self.boundingRect()
         if br.width() > 1000:
             raise Exception()
         sliced, coords = ROI.getArrayRegion(self, data, img, axes=axes, fromBoundingRect=True, **kwds)
-        #print len(sliced), axes[0], axes[1]
+        #print np.shape(sliced), sliced # len(sliced), axes[0], axes[1]
         if img.axisOrder == 'col-major':
             mask = self.renderShapeMask(sliced.shape[axes[0]], sliced.shape[axes[1]])
             #mask = self.renderShapeMask(sliced[axes[0]-1], sliced[axes[1]-1])
@@ -2062,7 +2063,11 @@ class PolyLineROI(ROI):
             mask = self.renderShapeMask(sliced.shape[axes[1]], sliced.shape[axes[0]])
             #mask = self.renderShapeMask(sliced[axes[1]-1], sliced[axes[0]-1])
             mask = mask.T
-            
+
+        # convert all '0's in the mask to another default value specified by kwds['defaultValue']
+        if kwds['defaultValue']:
+            mask[mask==0.]=kwds['defaultValue']
+            #print np.shape(mask), mask
         # reshape mask to ensure it is applied to the correct data axes
         shape = [1] * data.ndim
         shape[axes[0]] = sliced.shape[axes[0]]

pyqtgraph/imageview/ImageView.py
@@ -441,12 +441,12 @@ class ImageView(QtGui.QWidget):
             if r['roi'].freeHandleMoved:
                 print 'freehandlemoved yes'
 
-        print hover, self.view.sceneBoundingRect().contains(posClick)
+        #print hover, self.view.sceneBoundingRect().contains(posClick)
         if (evt.buttons() == QtCore.Qt.MiddleButton) and (self.view.sceneBoundingRect().contains(posClick)) and (not hover):
-            print 'yes'
+            #print 'yes'
             posClick= evt.pos()
             mousePointC = self.view.mapSceneToView(posClick)
-            print 'mousePointC : ', mousePointC
+            #print 'mousePointC : ', mousePointC
             self.addROI(mousePointC)
 
     def addROI(self,loc):
@@ -454,7 +454,7 @@ class ImageView(QtGui.QWidget):
         center = loc
         size = [20, 20]
         #pts = [center, center + Point(0, size[1]), center + Point(size[1], size[0]), center + Point(size[0], 0)]
-        pts = [[0,0],[0,10],[10,10],[10,0]]
+        pts = [[0,0],[0,2],[2,2],[2,0]]
         #roi = RectROI(loc,size,pen=pen, removable=True) #PolyLineROI(pts, pen=pen, closed=True, removable=True)
         roi = PolyLineROI(pts, pos=loc,pen=pen, closed=True, removable=True)
         #roi.setZValue(20)
@@ -693,21 +693,23 @@ class ImageView(QtGui.QWidget):
             #    # roiPlot.removeItem(r['plot'])
             #    self.ROIs.remove(r)
             #    break
-            data, coords = r['roi'].getArrayRegion(image.view(np.ndarray), self.imageItem, axes, returnMappedCoords=True)
+            data, coords = r['roi'].getArrayRegion(image.view(np.ndarray), self.imageItem, axes,returnMappedCoords=True,defaultValue=np.nan)
             #data = r['roi'].getArrayRegion(image.view(np.ndarray), self.imageItem, axes, returnMappedCoords=True)
+            #print np.shape(data), data
             if data is None:
                 return
             #print 'coords :', coords
             # Convert extracted data into 1D plot data
             if self.axes['t'] is None:
                 # Average across y-axis of ROI
-                data = data.mean(axis=axes[1])
+                data = np.nanmean(data,axis=axes[1])
                 coords = coords[:,:,0] - coords[:,0:1,0]
                 xvals = (coords**2).sum(axis=0) ** 0.5
 
             else:
                 # Average data within entire ROI for each frame
-                data = data.mean(axis=max(axes)).mean(axis=min(axes))
+                #data = np.nanmean(data,axis=max(axes)).nanmean(axis=min(axes))
+                data = np.nanmean(np.nanmean(data,axis=max(axes)),axis=min(axes))
                 xvals = self.tVals
 
             #print data.ndim

roiAnalysis.py
@@ -27,13 +27,13 @@ win.show()
 win.setWindowTitle('pyqtgraph : augmented ImageView')
 
 ## Create strcutured 3D data
-decay = np.exp(-np.linspace(0,2,100))[:,np.newaxis,np.newaxis]
-data = np.ones((100,200,200))
+decay = np.exp(-np.linspace(0,2,10))[:,np.newaxis,np.newaxis]
+data = np.ones((10,20,20))
 data += 2
-data[:,:100,:100] = 1
-data[:,100:,:100] = 2
-data[:,:100,100:] = 3
-data[:,100:,100:] = 4
+data[:,:10,:10] = 1
+data[:,10:,:10] = 2
+data[:,:10,10:] = 3
+data[:,10:,10:] = 4
 
 data = data * decay
 